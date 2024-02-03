b848d2858d136eb4dda099937f596ba1afe72a4a
petrsokerin <petya4343@mail.ru>
2024-02-03 20:27:49 +0300

    first commit

---
+++ download_data.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from tqdm import tqdm
+
+from src.finam_download import get
+
+def get_tickers_sp500():
+    table = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    sp500_df = table[0]
+
+    # Extract the tickers column
+    tickers = sp500_df['Symbol'].tolist()
+    return tickers
+    
+def get_all_sp500_data():
+    sp500_tickers = get_tickers_sp500()
+    failed_list = []
+    for ticker in tqdm(sp500_tickers):
+        try:
+            get(ticker, 'M1', f'data/ticker_data/{ticker}.csv')
+        except:
+            print(f'Error witn ticker {ticker}')
+            failed_list.append(ticker)
+
+    print(len(failed_list), failed_list)
+
+if __name__ == '__main__':
+    get_all_sp500_data()
---
+++ src/finam_download.py
@@ -0,0 +1,123 @@
+import sys
+from urllib.parse   import urlencode
+from urllib.request import urlopen, Request
+from datetime       import timedelta, datetime
+from time           import sleep
+import yaml
+
+def get_assets_timestemps_codes():
+    with open('configs/downloads/ticker_codes.yaml', 'r') as f:
+        assets = yaml.load(f, Loader=yaml.FullLoader)
+
+    with open('configs/downloads/timeframes.yaml', 'r') as f:
+        timeframes = yaml.load(f, Loader=yaml.FullLoader)
+    
+    return assets, timeframes
+
+def get_piece(asset, timeframe, first, last, path) :
+
+    fout = open(path, "a")
+    assets, timeframes = get_assets_timestemps_codes()
+
+    
+    try :
+
+        domain = "http://export.finam.ru/"
+        code = assets[asset]
+
+        properties = urlencode([
+		    ("market",    0),                       # Тип рынка
+		    ("em",        code),           # Код актива
+	            ("code",      asset),                   # Имя актива
+                    ("apply",     0),                       # Избранное
+		    ("df",        first.day),               # Начальная дата, номер дня (1-31)
+		    ("mf",        first.month - 1),         # Начальная дата, номер месяца (0-11)
+		    ("yf",        first.year),              # Начальная дата, год
+		    ("from",      first),                   # Начальная дата
+		    ("dt",        last.day),                # Конечная дата, номер дня (1-31)
+		    ("mt",        last.month - 1),          # Конечная дата, номер месяца (0-11)
+		    ("yt",        last.year),               # Конечная дата, год
+		    ("to",        last),                    # Конечная дата
+		    ("p",         timeframes[timeframe]),   # Таймфрейм
+		    ("f",         asset + "_" + timeframe), # Имя сформированного файла
+		    ("e",         ".txt"),                  # Расширение сформированного файла
+		    ("cn",        asset),                   # Имя актива	
+		    ("dtf",       1),                       # Формат даты
+		    ("tmf",       1),                       # Формат времени
+		    ("MSOR",      0),                       # Время свечи
+		    ("mstime",    "on"),                    # Московское время	
+		    ("mstimever", 1),                       # Коррекция часового пояса	
+		    ("sep",       1),                       # Разделитель полей
+		    ("sep2",      1),                       # Разделитель разрядов
+		    ("datf",      5),                       # Формат записи в файл
+		    ("at",        0)                        # Заголовки столбцов
+        ])
+
+        url = domain + asset + "_" + timeframe + ".txt?" + properties
+        
+        request = Request(url, headers = {"User-Agent": "Mozilla/5.0"}) # ?
+
+        text = urlopen(request).readlines()
+        
+        lines = []
+        
+        for line in text:
+            lines.append(line.strip().decode("utf-8") + "\n")
+
+        for line in reversed(lines):
+            fout.write(line)	
+
+    except:
+        print("Exception: ", sys.exc_info()[0])
+        raise
+
+    finally:
+        fout.close()
+
+def get(asset, timeframe, path) :
+
+    try:
+        
+        fout = open(path, "w")
+        
+        fout.close()
+
+        total = 365 * 1
+        batch = 365 * 1
+
+        first = datetime.now().date() - timedelta(days = 1)
+        last  = first                 - timedelta(days = batch)
+        
+        for i in range(0, total, batch):
+            
+            get_piece(asset, timeframe, last, first, path)
+
+            first = last  - timedelta(days = 2)
+            last  = first - timedelta(days = batch)
+
+            sleep(1)
+
+    except:
+        print("Exception: ", sys.exc_info()[0])
+        raise
+    
+def get_for_levels(asset, timeframe, path) :
+
+    try:
+        
+        fout = open(path, "w")
+        
+        fout.close()
+
+        total = 365 * 10
+
+        first = datetime.now().date() - timedelta(days = 1)
+        last  = first                 - timedelta(days = total)
+            
+        get_piece(asset, timeframe, last, first, path)
+
+        sleep(1)
+
+    except:
+        print("Exception: ", sys.exc_info()[0])
+        raise
